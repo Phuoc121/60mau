ee33bdcdcc69541f74b8fd6024f128e608005bae
Phuoc121 <phuoc_dpm225458@student.agu.edu.vn>
2025-11-03 08:39:10 +0700

    Add files via upload

---
+++ Add Parameter solution/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Customer
+{
+    public string GetContact(DateTime date)
+    {
+        // Bây giờ có thể trả về thông tin liên hệ dựa trên ngày
+        if (date.DayOfWeek == DayOfWeek.Sunday)
+            return "Contact closed on Sunday.";
+        else
+            return "Contact available on " + date.ToShortDateString();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Customer customer = new Customer();
+        Console.WriteLine(customer.GetContact(DateTime.Now));
+    }
+}
